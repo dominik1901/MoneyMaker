1672bb1e1ba038ec42c54a2e29c33636b5b9f5bd
philcoder10 <philipp.schoech@gmail.com>
2017-10-24 15:45:02 +0200

    dropdown list

--- app/src/main/java/at/gad/moneymanager/MainActivity.java
+++ app/src/main/java/at/gad/moneymanager/MainActivity.java
@@ -1,13 +1,60 @@
+//https://www.tutorialspoint.com/android/android_spinner_control.htm
 package at.gad.moneymanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        List<String> categories = new ArrayList<String>();
+
+        categories.add("Kategorie");
+        categories.add("Lol test");
+        categories.add("amk");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+
+
+
     }
+
+
+
+
 }
